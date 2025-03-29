de0622fb7f01723171b0d377e5defcf3b160490e
agrathwohl <andrew@grathwohl.me>
2025-03-29 00:37:19 -0500

    dang

--- app/api/get-post/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { getPostData } from "../lib/posts";
-
-export const runtime = "edge";
-
-export async function GET(request: Request) {
-  const data = getPostData("2020-08-14-Web-Audio");
-  return NextResponse.json({ data });
-}
